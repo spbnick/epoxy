807fd49b314b48f6f4ac8231bcc280063ba2aa9d
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2012-12-08 00:08:53 +0200

    Cleanup bt_status.sh a little
    Remove -i option from declarations in bt_status.sh.
    Slight formatting improvement.

--- lib/bt_status.sh
+++ lib/bt_status.sh
@@ -16,19 +16,19 @@ declare -r _BT_STATUS_SH=
 # Status codes
 #
 # Skipped
-declare -r -i BT_STATUS_SKIPPED=1
+declare -r BT_STATUS_SKIPPED=1
 # Passed
-declare -r -i BT_STATUS_PASSED=2
+declare -r BT_STATUS_PASSED=2
 # Waived
-declare -r -i BT_STATUS_WAIVED=3
+declare -r BT_STATUS_WAIVED=3
 # Failed
-declare -r -i BT_STATUS_FAILED=4
+declare -r BT_STATUS_FAILED=4
 # A setup error occurred
-declare -r -i BT_STATUS_ERRORED=5
+declare -r BT_STATUS_ERRORED=5
 # A cleanup error occurred
-declare -r -i BT_STATUS_PANICKED=6
+declare -r BT_STATUS_PANICKED=6
 # A coding error occurred
-declare -r -i BT_STATUS_ABORTED=7
+declare -r BT_STATUS_ABORTED=7
 
 # Check if a status code is valid
 # Args: status
@@ -49,13 +49,13 @@ function bt_status_to_str()
     declare -r status="$1"
 
     case "$status" in
-        $BT_STATUS_SKIPPED) echo SKIPPED;;
-        $BT_STATUS_PASSED) echo PASSED;;
-        $BT_STATUS_WAIVED) echo WAIVED;;
-        $BT_STATUS_FAILED) echo FAILED;;
-        $BT_STATUS_ERRORED) echo ERRORED;;
-        $BT_STATUS_PANICKED) echo PANICKED;;
-        $BT_STATUS_ABORTED) echo ABORTED;;
+        $BT_STATUS_SKIPPED)     echo SKIPPED;;
+        $BT_STATUS_PASSED)      echo PASSED;;
+        $BT_STATUS_WAIVED)      echo WAIVED;;
+        $BT_STATUS_FAILED)      echo FAILED;;
+        $BT_STATUS_ERRORED)     echo ERRORED;;
+        $BT_STATUS_PANICKED)    echo PANICKED;;
+        $BT_STATUS_ABORTED)     echo ABORTED;;
         *) bt_abort "Invalid status code: $status";;
     esac
 }
